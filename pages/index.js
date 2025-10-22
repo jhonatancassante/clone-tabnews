e55375e6a05ed91ca2a3aa34c9cedb36946388ea
Jhonatan Casssante de Brito <jhonatan.cassante@live.com>
2025-10-22 17:42:19 +0000

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,50 @@
 function Home() {
   return (
     <div
-        style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            width: '100%',
-            backgroundColor: '#f3f4f6',
-        }}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100%",
+        backgroundColor: "#f3f4f6",
+      }}
     >
-    <div
+      <div
         style={{
-            fontSize: '2.5rem',
-            textTransform: 'uppercase',
-            fontWeight: 'bold',
-            color: '#111827',
-            padding: '2.5rem 3rem',
-            backgroundColor: '#fff',
-            borderRadius: '0.375rem',
-            boxShadow: '0 1px 3px rgba(0, 0, 0, 0.1)',
+          fontSize: "2.5rem",
+          textTransform: "uppercase",
+          fontWeight: "bold",
+          color: "#111827",
+          padding: "2.5rem 3rem",
+          backgroundColor: "#fff",
+          borderRadius: "0.375rem",
+          boxShadow: "0 1px 3px rgba(0, 0, 0, 0.1)",
         }}
-    >
+      >
         Don't Panic
       </div>
       <div
         style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            width: '100%',
-            position: 'absolute',
-            fontSize: '7rem',
-            color: 'transparent',
-            zIndex: 100,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100%",
+          position: "absolute",
+          fontSize: "7rem",
+          color: "transparent",
+          zIndex: 100,
         }}
-      >42</div>
+      >
+        42
+      </div>
       <div
         style={{
-            position: 'absolute',
-            bottom: '1rem',
-            fontSize: '.75rem',
-            color: '#c5c5c5ff',
+          position: "absolute",
+          bottom: "1rem",
+          fontSize: ".75rem",
+          color: "#c5c5c5ff",
         }}
       >
         Select the text on screen to reveal the answer.
