562458e9e0a7da2eb8049f99e6c239d981f13a6c
Jhonatan Casssante de Brito <jhonatan.cassante@live.com>
2025-10-23 19:20:54 +0000

    mudança na forma como a mensagem secreta é exibida

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,64 @@
+import { useState } from "react";
+
 function Home() {
+  const [showMsg, setShowMsg] = useState(false);
+
   return (
-    <>
-      <style>
-        {`
-          .highlightable {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            width: 100%;
-            position: absolute;
-            font-size: 7rem;
-            color: rgba(0, 0, 0, 0.001);
-            z-index: 100;
-            user-select: text;
-            text-align: center;
-          }
-          .highlightable::selection {
-            background: black !important;
-            color: white !important;
-          }
-          .highlightable::-moz-selection {
-            background: black !important;
-            color: white !important;
-          }
-        `}
-      </style>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100%",
+        backgroundColor: "#f3f4f6",
+      }}
+    >
       <div
         style={{
-          display: "flex",
+          fontSize: "2.5rem",
+          textTransform: "uppercase",
+          fontWeight: "bold",
+          color: "#111827",
+          padding: "2.5rem 3rem",
+          backgroundColor: "#fff",
+          borderRadius: "0.375rem",
+          boxShadow: "0 1px 3px rgba(0, 0, 0, 0.1)",
+          cursor: "pointer",
+        }}
+        onClick={() => setShowMsg((prev) => !prev)}
+      >
+        Don't Panic
+      </div>
+      <div
+        style={{
+          display: showMsg ? "flex" : "none",
           justifyContent: "center",
           alignItems: "center",
-          height: "100vh",
-          width: "100%",
-          backgroundColor: "#f3f4f6",
+          position: "absolute",
+          fontSize: "7rem",
+          color: "white",
+          background: "black",
+          zIndex: "100",
+          userSelect: "text",
+          textAlign: "center",
+          cursor: "pointer",
+        }}
+        onClick={() => setShowMsg((prev) => !prev)}
+      >
+        42
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          bottom: "1rem",
+          fontSize: ".75rem",
+          color: "#c5c5c5ff",
         }}
       >
-        <div
-          style={{
-            fontSize: "2.5rem",
-            textTransform: "uppercase",
-            fontWeight: "bold",
-            color: "#111827",
-            padding: "2.5rem 3rem",
-            backgroundColor: "#fff",
-            borderRadius: "0.375rem",
-            boxShadow: "0 1px 3px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          Don't Panic
-        </div>
-        <div
-          className="highlightable"
-        >
-          42
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            bottom: "1rem",
-            fontSize: ".75rem",
-            color: "#c5c5c5ff",
-          }}
-        >
-          Select the text on screen to reveal the answer.
-        </div>
-      </div></>
+        Click the text on screen to reveal the answer.
+      </div>
+    </div>
   );
 }
 
