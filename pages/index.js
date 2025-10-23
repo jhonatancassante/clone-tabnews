251512951f862e567ba2833bd09c3a59490ca8a6
Jhonatan Casssante de Brito <jhonatan.cassante@live.com>
2025-10-23 17:29:33 +0000

    adicionada compatibilidade com mobile e estilizado a selação

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,18 @@
 function Home() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100%",
-        backgroundColor: "#f3f4f6",
-      }}
-    >
-      <div
-        style={{
-          fontSize: "2.5rem",
-          textTransform: "uppercase",
-          fontWeight: "bold",
-          color: "#111827",
-          padding: "2.5rem 3rem",
-          backgroundColor: "#fff",
-          borderRadius: "0.375rem",
-          boxShadow: "0 1px 3px rgba(0, 0, 0, 0.1)",
-        }}
-      >
-        Don't Panic
-      </div>
+    <>
+      <style>
+        {`
+          .highlightable::selection {
+            background: black;
+            color: white;
+          }
+          .highlightable::-moz-selection {
+            background: black;
+            color: white;
+          }
+        `}
+      </style>
       <div
         style={{
           display: "flex",
@@ -31,25 +20,51 @@ function Home() {
           alignItems: "center",
           height: "100vh",
           width: "100%",
-          position: "absolute",
-          fontSize: "7rem",
-          color: "transparent",
-          zIndex: 100,
+          backgroundColor: "#f3f4f6",
         }}
       >
-        42
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          bottom: "1rem",
-          fontSize: ".75rem",
-          color: "#c5c5c5ff",
-        }}
-      >
-        Select the text on screen to reveal the answer.
-      </div>
-    </div>
+        <div
+          style={{
+            fontSize: "2.5rem",
+            textTransform: "uppercase",
+            fontWeight: "bold",
+            color: "#111827",
+            padding: "2.5rem 3rem",
+            backgroundColor: "#fff",
+            borderRadius: "0.375rem",
+            boxShadow: "0 1px 3px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          Don't Panic
+        </div>
+        <div
+          className="highlightable"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            width: "100%",
+            position: "absolute",
+            fontSize: "7rem",
+            color: "transparent",
+            zIndex: 100,
+            userSelect: "text",
+          }}
+        >
+          42
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            bottom: "1rem",
+            fontSize: ".75rem",
+            color: "#c5c5c5ff",
+          }}
+        >
+          Select the text on screen to reveal the answer.
+        </div>
+      </div></>
   );
 }
 
