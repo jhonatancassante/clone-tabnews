383ce626ffb1636b6f7c5564375ef21363e13754
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2025-10-28 20:23:49 -0300

    ajusta o tratamento de erros na função de consulta do banco de dados

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === 'development' ? false : true,
   });
 
   console.log("Credenciais do Postgres:", {
@@ -14,7 +15,6 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
   });
 
   try {
@@ -23,6 +23,7 @@ async function query(queryObject) {
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
