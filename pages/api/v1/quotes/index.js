5c8a94485ae24e1f998d2a3e328d22abea97bc7e
jhonatancassante <jhonatan.cassante@live.com>
2026-03-28 16:14:45 -0300

    refactor: simplifica a criação de rotas utilizando encadeamento

--- pages/api/v1/quotes/index.js
+++ pages/api/v1/quotes/index.js
@@ -3,12 +3,10 @@ import controller from "@/infra/controller.js";
 import quote from "@/models/quote.js";
 import authorization from "@/models/authorization";
 
-const router = createRouter();
-
-router.use(controller.injectAnonymousOrUser);
-router.get(getHandler);
-
-export default router.handler(controller.errorHandlers);
+export default createRouter()
+  .use(controller.injectAnonymousOrUser)
+  .get(getHandler)
+  .handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
   const userTryingToGet = request.context.user;
